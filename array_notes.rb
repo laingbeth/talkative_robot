aa5d1ad315c87836798f17218ef562392fbe2b67
beth laing <laingbeth@gmail.com>
2015-01-15 15:30:44 -0500

    unknown changes

---
+++ array_notes.rb
@@ -0,0 +1,17 @@
+# array
+# ordered collection
+
+# array = array.new
+# array = [ ]
+
+# colors[0]
+# colors = [ "blue", "red", "yellow", "green"]
+
+# colors.index('green')
+
+cereals = [ ]
+cereals = [ "cheerios", "frosted flakes", "lucky charms", "trix", "coco puffs", "granola", "frosted mini wheats", "kix", "fruit loops", "corn flakes"]
+cereals ("coco puffs")
+# cereals(10) ="captin crunch peanut butter"
+# cereals.first = 
+# cereals.last =
---
+++ test_talkative_robot.rb
@@ -0,0 +1,84 @@
+# ##Lab/Homework - Talkative Robot
+# ###Add on to previous lab with:
+# Group previously defined if/else and case statements into methods
+# Have at least one method that does not need a param
+# Have at least one method that requires a param
+
+def get_name
+	puts "What is your name?"
+	return gets.chomp
+end
+name = get_name
+
+#user[:name] = get_name
+
+def get_age
+puts "How old are you?"
+return gets.chomp.to_i 
+end
+age = get_age
+
+puts "Hi #{name}, who is #{age} years old!"
+
+def get_gender
+	puts "what gender are you, M or F?"
+	return gets.chomp.capitalize
+end
+gender = get_gender
+
+puts "So, you're a #{gender}."
+
+if gender == "M"  
+	puts "Hi there, Champ" 
+else gender == "F"  
+	puts "Hello Gorgeous!" 
+end
+
+def fav_robot
+	puts "What would your favorite movie robot say right now?"
+end
+
+def olive_fav
+	puts "What is your favorite kind of olive, green or black?"
+	return gets.chomp
+end
+olive = olive_fav
+puts "I like black olives too" 				unless olive == "green" 
+puts "Ug, green olives are gross!" 			if olive == "green" 
+#puts "Yeah, olives are gross"				if olive == "neither"
+
+initial = name.chars.first.upcase 
+puts "Do you mind if I call you #{initial}?"
+
+#age = get_age
+
+if age < 75
+	puts "You'll be 75 year old in #{75 - age} years."
+elsif  age == 75 
+	puts "Hey, you're 75!"	
+elsif age > 75 
+	puts "You turned 75 #{75 - age} years ago!"
+else
+	puts "I don't recognize your age."
+end
+
+if gender == "F" && age <= 10 
+	puts "You're a young girl"
+elsif gender == "M" && age <= 10
+	puts "You're a young boy"
+elsif gender == "F" && age > 120
+	puts "You're a great-great grandmother"
+elsif gender == "M" && age > 120
+	puts "You're a great-great grandfather"
+end
+
+name.capitalize!
+puts name
+
+puts "Hey #{name.upcase}, where are you going!?"
+puts 'Hey "Dude", What\'s up?' 
+
+puts fav_robot
+
+puts age < 25 ? "You're just a youngster!" : "With age comes wisdom!"
+puts name == "Ernest" ? "I knew it was you!" : "Sorry, your're not Ernest."
